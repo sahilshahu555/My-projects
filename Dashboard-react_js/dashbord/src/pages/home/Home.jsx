c12c6e10337d1c3fa7ce9414a17a8150e36d2e66
Sahil Shahu <sahilshahu555@gmail.com>
2023-10-19 16:47:21 +0530

    edited chartBox and topBox

--- Dashboard-react_js/dashbord/src/pages/home/Home.jsx
+++ Dashboard-react_js/dashbord/src/pages/home/Home.jsx
@@ -3,14 +3,42 @@ import "./home.scss"
 import TopBox from '../../components/TopBox/TopBox'
 import ChartBox from '../../components/ChartBox/ChartBox'
 const Home = () => {
+  const box2Data={
+    title:"Total Users",
+    points:"11.238",
+    icon:"user.svg",
+    per:"45"
+  }
+  const box3Data={
+    title:"Total Products",
+    points:"538",
+    icon:"product.svg",
+    per:"78"
+  }
+  const box5Data={
+    title:"Total Ratio",
+    points:"3.2",
+    icon:"chart.svg",
+    per:"55"
+  }
+  const box6Data={
+    title:"Total Revenue",
+    points:"$56.30",
+    icon:"revenueIcon.svg",
+    per:"-24"
+  }
   return (
     <div className='home'>
       <div className="box box1"><TopBox/></div>
-      <div className="box box2"><ChartBox/></div>
-      <div className="box box3"><ChartBox/></div>
+      <div className="box box2"><ChartBox title={box2Data.title} points={box2Data.points}
+       icon={box2Data.icon} per={box2Data.per}/></div>
+      <div className="box box3"><ChartBox title={box3Data.title} points={box3Data.points}
+       icon={box3Data.icon} per={box3Data.per}/></div>
       <div className="box box4"></div>
-      <div className="box box5"><ChartBox/></div>
-      <div className="box box6"><ChartBox/></div>
+      <div className="box box5"><ChartBox   title={box5Data.title} points={box5Data.points}
+       icon={box5Data.icon} per={box5Data.per}/></div>
+      <div className="box box6"><ChartBox  title={box6Data.title} points={box6Data.points}
+       icon={box6Data.icon} per={box6Data.per} /></div>
       <div className="box box7">box7</div>
       <div className="box box8">box8</div>
       <div className="box box9">box9</div>
