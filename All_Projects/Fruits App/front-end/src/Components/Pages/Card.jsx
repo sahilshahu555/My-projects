59f90470732bbc703bf075d29cfed23770e154a3
sahilshahu555 <sahilshahu555@gmail.com>
2024-01-18 14:16:07 +0530

    edited card home

--- All_Projects/Fruits App/front-end/src/Components/Pages/Card.jsx
+++ All_Projects/Fruits App/front-end/src/Components/Pages/Card.jsx
@@ -2,12 +2,23 @@
 
 const Card = ({elm}) => {
   return (
-    <div className="flex flex-col justify-around items-center border-2 rounded-2xl 
-      bg-red-300
+    <div className="flex flex-col justify-around items-center gap-2
+      shadow-lg shadow-gray-400/100 ... rounded-tr-3xl rounded-bl-3xl
+    transition ease-in-out delay-150 bg-yellow-300 hover:-translate-y-1 hover:scale-110 hover:bg-white duration-300 ...
     ">
-      <img src={elm.image} alt={elm.id} className="w-60 h-60 rounded-xl"  />
-      <h1 className="text-gray-500 font-semibold">{elm.name}</h1>
-      <p className="text-gray-500 font-medium"> ₹. {elm.price}</p>
+      <img src={elm.image} alt={elm.id} className="w-60 h-60 rounded-tr-2xl"  />
+      <h1 className="text-gray-500 font-bold text-xl m-1">{elm.name}</h1>
+      <div className="flex justify-around items-center gap-10 m-2">
+        <p className="text-gray-500 font-medium text-lg"> ₹. {elm.price}</p>
+        <button 
+        className="text-gray-500 font-medium bg-red-300 py-1 px-6 m-1
+                   rounded-xl border-2 hover:bg-yellow-400 curser-pointer
+                   shadow-lg shadow-gray-100
+                   "
+        
+        >Buy</button>
+      </div>
+      
     </div>
   )
 }
--- All_Projects/Fruits App/front-end/src/Components/Pages/Home.jsx
+++ All_Projects/Fruits App/front-end/src/Components/Pages/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
     <div >
       <div>
         <h1
-        className="text-center text-gray-500 font-bold text-xl"
+        className="text-center text-gray-500 font-bold text-xl bg-white w-36 m-auto py-1 px-2 rounded-tr-xl rounded-bl-xl"
         >Fresh Fruits</h1>
       </div>
-      <div className="grid sm:grid-cols-3 md:grid-cols-3  lg:grid-cols-5 gap-10 m-5 ">
+      <div className="grid  md:grid-cols-3  lg:grid-cols-5 gap-10 m-5 ">
       {fruits?.map((elm,i)=><Card elm={elm} key={i}/>)}
       </div>
     </div>
