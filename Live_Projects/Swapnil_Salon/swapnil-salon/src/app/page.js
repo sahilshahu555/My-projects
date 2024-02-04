d0c41627eb3c68ecafc4ea11b8d1fb38f9df805c
sahilshahu555 <sahilshahu555@gmail.com>
2024-02-04 17:44:35 +0530

    ed

--- Live_Projects/Swapnil_Salon/swapnil-salon/src/app/page.js
+++ Live_Projects/Swapnil_Salon/swapnil-salon/src/app/page.js
@@ -21,7 +21,7 @@ useEffect(()=>{
     <main className="flex justify-between flex-col items-center  ">
         <div className=" w-full">
             <div className="absolute -z-10 w-full ">
-              <Image src="https://png.pngtree.com/thumb_back/fh260/background/20230516/pngtree-pair-of-black-scissors-next-to-a-hair-image_2570258.jpg"alt="background" className='w-full  h-[28rem] lg:h-[32rem] ' width={1000} height={1000}/>
+              <Image src="https://png.pngtree.com/thumb_back/fh260/background/20230516/pngtree-pair-of-black-scissors-next-to-a-hair-image_2570258.jpg"alt="background" className='w-full  h-[28rem] lg:h-[32rem] ' width={900} height={1000}/>
             </div>
             {/*  "https://png.pngtree.com/thumb_back/fh260/background/20230516/pngtree-pair-of-black-scissors-next-to-a-hair-image_2570258.jpg" */}
         </div>
@@ -30,7 +30,7 @@ useEffect(()=>{
              <ServiceHome/>
        {/* for making background Image */}
                   <div className="absolute -z-10 w-full ">
-                    <Image src="https://images.unsplash.com/photo-1520338661084-680395057c93?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aGFpciUyMGN1dHRpbmd8ZW58MHx8MHx8fDA%3D"alt="background" className='w-full  h-[38rem] lg:h-[35rem] ' width={1000} height={100}/>
+                    <Image src="https://images.unsplash.com/photo-1520338661084-680395057c93?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aGFpciUyMGN1dHRpbmd8ZW58MHx8MHx8fDA%3D"alt="background" className='w-full  h-[38rem] lg:h-[35rem] ' width={900} height={100}/>
                   </div>
                   {/* "https://images.unsplash.com/photo-1520338661084-680395057c93?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aGFpciUyMGN1dHRpbmd8ZW58MHx8MHx8fDA%3D" */}
        {/* for making background Image */}
