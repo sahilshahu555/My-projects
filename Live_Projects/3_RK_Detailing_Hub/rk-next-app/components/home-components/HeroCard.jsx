b8eae0d91d436ffdeaf025913d942a32f607e319
sahilshahu555 <sahilshahu555@gmail.com>
2024-02-11 02:28:21 +0530

    edited hero section

--- Live_Projects/3_RK_Detailing_Hub/rk-next-app/components/home-components/HeroCard.jsx
+++ Live_Projects/3_RK_Detailing_Hub/rk-next-app/components/home-components/HeroCard.jsx
@@ -10,7 +10,7 @@ const HeroCard = ({elm}) => {
             <div className="Main-Home font-sans  text-white flex flex-col      text-center     justify-center  items-center   gap-5 md:gap-7   background h-[20rem] md:h-[32rem] ">
                 <h3 className="text-lg md:text-xl text-cyan-400 font-semibold ">{elm.title}</h3>
                 <h1 className="text-4xl md:text-[5rem] leading-none font-bold w-5/6">{elm.main}</h1>
-                <p className='px-2 md:px-0'>{elm.desc}</p>
+                {/* <p className='px-2 md:px-0'>{elm.desc}</p> */}
                 <button className={`bg-cyan-400 hover:bg-cyan-300 hover:text-black font-bold mt-5 p-2 rounded-full w-36 `}>Explore More ! </button>
 
             </div>
